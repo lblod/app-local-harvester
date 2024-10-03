426da6a40cb0ca3f49953055f8d74297b0f7851d
Nordine Bittich <contact@bittich.be>
2024-10-03 17:58:39 +0200

    use derived from

--- make-polygon.js
+++ make-polygon.js
@@ -3,9 +3,9 @@ if (args.length < 3) {
   console.error("usage <lon> <lat> <dist-in-meters>");
   process.exit(-1);
 }
-const lon = parseFloat(args[0])
-const lat = parseFloat(args[1])
-const dist = parseFloat(args[2])
+const lon = parseFloat(args[0]);
+const lat = parseFloat(args[1]);
+const dist = parseFloat(args[2]);
 const minLon = lon - dist;
 const maxLon = lon + dist;
 const minLat = lat - dist;
@@ -17,10 +17,12 @@ PREFIX geor: <http://www.opengis.net/def/rule/geosparql/>
 PREFIX sf: <http://www.opengis.net/ont/sf#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-SELECT distinct ?place
+SELECT distinct ?place ?agenda
 WHERE {
   ?geometry geo:asWKT ?wkt .
   ?loc a <http://www.w3.org/ns/locn#Location>; <http://www.w3.org/ns/locn#geometry> ?geometry;<http://www.w3.org/2000/01/rdf-schema#label> ?place.
-  FILTER(geof:sfWithin(?wkt, "<http://www.opengis.net/def/crs/EPSG/0/31370> POLYGON((${minLon} ${minLat}, ${minLon} ${maxLat}, ${maxLon} ${maxLat}, ${maxLon} ${minLat}, ${minLon} ${minLat}))"^^geo:wktLiteral))
+  ?loc prov:wasDerivedFrom ?geoDerivedFrom.
+  ?agenda a besluit:BehandelingVanAgendapunt; prov:wasDerivedFrom ?aDerived.
+  FILTER(geof:sfWithin(?wkt, "<http://www.opengis.net/def/crs/EPSG/0/31370> POLYGON((${minLon} ${minLat}, ${minLon} ${maxLat}, ${maxLon} ${maxLat}, ${maxLon} ${minLat}, ${minLon} ${minLat}))"^^geo:wktLiteral) && ?aDerived = ?geoDerivedFrom)
  }
-`)
+`);
